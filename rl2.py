412f696220242e24aaffb99fa536fb19a8bf514a
Ohad <ohadkorenok@gmail.com>
2020-01-07 19:56:09 +0200

    yoav changes

--- rl2.py
+++ rl2.py
@@ -9,21 +9,21 @@ env = gym.make("MountainCar-v0")
 initial_state = env.reset()
 
 
-def choose_action(state,weight_vector, epsilon):
+def choose_action(state, weight_vector, epsilon):
     greedy = random() > epsilon
     if greedy:
         curr_pos = state[0]
         curr_vel = state[1]
-        actions_state_value = [np.dot(state_to_feature_function(curr_pos, curr_vel, i), weight_vector) for i in
-                               [0, 1, 2]]
-        max = actions_state_value[0]
-        chosen_action = 0
-        for index, a in enumerate(actions_state_value):
-            if a > max:
-                max = a
-                chosen_action = index
+        left = np.dot(state_to_feature_function(curr_pos, curr_vel, 0), weight_vector)
+        stay = np.dot(state_to_feature_function(curr_pos, curr_vel, 1), weight_vector)
+        right = np.dot(state_to_feature_function(curr_pos, curr_vel, 2), weight_vector)
+        actions = [left, stay, right]
+
+        chosen_action = np.argmax(actions)
     else:
         chosen_action = int(random() * 3)
+        if chosen_action == 3:
+            chosen_action = 2
 
     return chosen_action
 
@@ -31,44 +31,45 @@ def choose_action(state,weight_vector, epsilon):
 def choose_greedy_action(state, weight_vector):
     curr_pos = state[0]
     curr_vel = state[1]
-    actions_state_value = [np.dot(state_to_feature_function(curr_pos, curr_vel, i), weight_vector) for i in
-                           [0, 1, 2]]
-    max = actions_state_value[0]
-    chosen_action = 0
-    for index, a in enumerate(actions_state_value):
-        if a > max:
-            max = a
-            chosen_action = index
+    left = np.dot(state_to_feature_function(curr_pos, curr_vel, 0), weight_vector)
+    stay = np.dot(state_to_feature_function(curr_pos, curr_vel, 1), weight_vector)
+    right = np.dot(state_to_feature_function(curr_pos, curr_vel, 2), weight_vector)
+    actions = [left, stay, right]
+    max_value = max(actions)
+    max_index = np.argmax(actions)
 
-    return (max, chosen_action)
+    return (max_value, max_index)
 
 
 def state_to_feature_function(position, velocity, action):
-    if position == 0.6:
-        position = 0.59
-    if velocity == 0.07:
-        velocity = 0.069
-    if velocity == -0.07:
-        velocity = -0.069
-    position_index = math.floor(((math.floor(position * 10) / 10) + 1.2) * 10)
-    velocity_index = math.floor(((math.floor(velocity * 100) / 100) + 0.07) * 100) + 18
-    action_index = action + 32
-    features = [0] * 35
-    features[position_index] = 1
-    features[velocity_index] = 1
-    features[action_index] = 1
 
-    # print("intial state is: {} , vel_index = {} action_index = {} , position_index = {}".
-    #       format(initial_state, velocity_index, action_index, position_index))
+    position_index = math.floor(position * 10) + 12
+    velocity_index = math.floor(velocity * 100) + 7
+    if position >= 0.6:
+        position_index = 17
+    if velocity <= -0.07:
+        velocity_index = 0
+    if velocity >= 0.07:
+        velocity_index = 13
+
+    table_index = position_index * 42 + velocity_index*3 + action
+    features = [0] * 756
+    features[table_index] = 1
     return features
 
-def dec_epsilon(epsilon_var):
-    return epsilon_var-0.1 if epsilon_var > 0.1 else epsilon_var
 
+def dec_epsilon(epsilon_var):
+    new_epsilon = epsilon_var - 0.01
+    if new_epsilon > 0.01:
+        return new_epsilon
+    else:
+        return 0.01
 
 def qlearning(weight_vector):
     """Q Learning"""
-    epsilon = 0.5
+    means = []
+    temp_arr = []
+    epsilon = 0.1
     done = False
     action = 0, 1, 2
     number_of_episodes = 0
@@ -79,16 +80,18 @@ def qlearning(weight_vector):
         episode_done = False
         num_of_steps = 0
         while not episode_done:
-            chosen_action = choose_action(curr_state,weight_vector, epsilon)  # chosen
+            chosen_action = choose_action(curr_state, weight_vector, epsilon)  # chosen action = 0 -> left 1-> stay 2->right
             curr_feature_vector = state_to_feature_function(curr_state[0], curr_state[1], chosen_action)
-            if curr_feature_vector[17] == 1:
-                episode_done = True
             # curr_feature_vector = X(current state,chosen action)
+
+            if curr_state[0] >= 0.5:
+                episode_done = True
+
             curr_as_value = np.dot(curr_feature_vector, weight_vector)
             # curr_as_value = q(current state, chosen action, w)
             next_state, curr_reward, dont_care, addition_inf = env.step(chosen_action)
             # env.render()
-            ga_value, ga_index = choose_greedy_action(next_state,weight_vector)
+            ga_value, ga_index = choose_greedy_action(next_state, weight_vector)
             # ga_value = greedy action value = q(next state, optimal action, w)
             # ga_index = index of greedy action chosen
             delta_w = np.dot(curr_feature_vector,
@@ -99,15 +102,19 @@ def qlearning(weight_vector):
             if num_of_steps == 1000:
                 episode_done = True
 
-        if number_of_episodes % 100 == 0:
-            print ("run number= ", number_of_episodes , "num of steps = ", num_of_steps)
-        if number_of_episodes % 100 == 0:
+        if number_of_episodes % 75 == 0:
             epsilon = dec_epsilon(epsilon)
+        if number_of_episodes % 10 == 0:
+            temp_arr.append(num_of_steps)
+        if number_of_episodes % 100 == 0:
+            means.append(np.mean(temp_arr))
+            temp_arr.clear()
+        if number_of_episodes % 10 == 0:
+            print("\nrun number= ", number_of_episodes, "num of steps = ", num_of_steps , "\nweights: ",weight_vector)
         number_of_episodes += 1
-        if number_of_episodes == 100000:
+        if number_of_episodes == 15000:
+            print(means)
             done = True
-            env.close()
+            # env.close()
 
-
-
-qlearning([0] * 35)
+qlearning([0] * 756)
