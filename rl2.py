43fdff9e5ccddb74774b509b82301410b825b325
Ohad <ohadkorenok@gmail.com>
2020-01-07 19:52:17 +0200

    my changes

--- rl2.py
+++ rl2.py
@@ -3,13 +3,15 @@ import math
 import numpy as np
 from random import random
 
+position_mappers = [-1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -.2, -.1, 0, .1, .2, .3, .4, .5]
+velocity_mappers = [-0.07, -0.06, -0.05, -0.04, -0.03, -0.02, -0.01, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06]
 learning_rate = 0.7
-discount_factor = 0.95
+discount_factor = 0.99
 env = gym.make("MountainCar-v0")
 initial_state = env.reset()
 
 
-def choose_action(state,weight_vector, epsilon):
+def choose_action(state, weight_vector, epsilon):
     greedy = random() > epsilon
     if greedy:
         curr_pos = state[0]
@@ -43,15 +45,21 @@ def choose_greedy_action(state, weight_vector):
     return (max, chosen_action)
 
 
+def get_mapper_index(mappers, value, offset=0):
+    last_index = -1
+    for index, element in enumerate(mappers):
+        if value < element:
+            return offset + last_index
+        else:
+            last_index += 1
+    return offset + last_index
+
+
 def state_to_feature_function(position, velocity, action):
-    if position == 0.6:
-        position = 0.59
-    if velocity == 0.07:
-        velocity = 0.069
-    if velocity == -0.07:
-        velocity = -0.069
-    position_index = math.floor(((math.floor(position * 10) / 10) + 1.2) * 10)
-    velocity_index = math.floor(((math.floor(velocity * 100) / 100) + 0.07) * 100) + 18
+    position_index = get_mapper_index(position_mappers, position)
+    velocity_index = get_mapper_index(velocity_mappers, velocity, 18)
+    # position_index = math.floor(((math.floor(position * 10) / 10) + 1.2) * 10)
+    # velocity_index = math.floor(((math.floor(velocity * 100) / 100) + 0.07) * 100) + 18
     action_index = action + 32
     features = [0] * 35
     features[position_index] = 1
@@ -62,13 +70,14 @@ def state_to_feature_function(position, velocity, action):
     #       format(initial_state, velocity_index, action_index, position_index))
     return features
 
+
 def dec_epsilon(epsilon_var):
-    return epsilon_var-0.1 if epsilon_var > 0.1 else epsilon_var
+    return epsilon_var - 0.05 if epsilon_var > 0.1 else epsilon_var
 
 
 def qlearning(weight_vector):
     """Q Learning"""
-    epsilon = 0.5
+    epsilon = 0.7
     done = False
     action = 0, 1, 2
     number_of_episodes = 0
@@ -79,7 +88,7 @@ def qlearning(weight_vector):
         episode_done = False
         num_of_steps = 0
         while not episode_done:
-            chosen_action = choose_action(curr_state,weight_vector, epsilon)  # chosen
+            chosen_action = choose_action(curr_state, weight_vector, epsilon)  # chosen
             curr_feature_vector = state_to_feature_function(curr_state[0], curr_state[1], chosen_action)
             if curr_feature_vector[17] == 1:
                 episode_done = True
@@ -88,19 +97,19 @@ def qlearning(weight_vector):
             # curr_as_value = q(current state, chosen action, w)
             next_state, curr_reward, dont_care, addition_inf = env.step(chosen_action)
             # env.render()
-            ga_value, ga_index = choose_greedy_action(next_state,weight_vector)
+            ga_value, ga_index = choose_greedy_action(next_state, weight_vector)
             # ga_value = greedy action value = q(next state, optimal action, w)
             # ga_index = index of greedy action chosen
             delta_w = np.dot(curr_feature_vector,
-                             learning_rate * (curr_reward + discount_factor * ga_value - curr_as_value))
+                             learning_rate * (curr_reward + (discount_factor * ga_value) - curr_as_value))
             weight_vector = np.add(delta_w, weight_vector)
             curr_state = next_state
             num_of_steps += 1
-            if num_of_steps == 1000:
+            if num_of_steps == 5000:
                 episode_done = True
 
-        if number_of_episodes % 100 == 0:
-            print ("run number= ", number_of_episodes , "num of steps = ", num_of_steps)
+        if number_of_episodes % 10 == 0:
+            print("run number= ", number_of_episodes, "num of steps = ", num_of_steps, "\n weights = ",weight_vector)
         if number_of_episodes % 100 == 0:
             epsilon = dec_epsilon(epsilon)
         number_of_episodes += 1
@@ -109,5 +118,9 @@ def qlearning(weight_vector):
             env.close()
 
 
-
+# test_1 = [-1.15+(i*0.1) for i in range(0, 18)]
+# test_2 = [-0.065+(i*0.01) for i in range(0,14)]
+# p = [i for i in map(lambda x: get_mapper_index(position_mappers, x), test_1)]
+# j = [j for j in map(lambda x: get_mapper_index(velocity_mappers, x, 18), test_2)]
 qlearning([0] * 35)
+
